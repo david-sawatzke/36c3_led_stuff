e7308609c5d791d2e28ca9cd210fdae1d109bc41
David Sawatzke <david-sawatzke@users.noreply.github.com>
2019-12-01 01:36:00 +0100

    Add "real" tail functionality

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,12 @@ use ws2812_nop_stm32f0 as ws2812;
 
 use crate::hal::delay::Delay;
 use crate::hal::prelude::*;
+use heapless::consts::*;
+use heapless::spsc::{Iter, Queue};
 use smart_leds::SmartLedsWrite;
 use smart_leds::RGB8;
+
+use core::iter::{Peekable, Rev};
 
 #[rtfm::app(device = stm32f0xx_hal::stm32, peripherals = true)]
 const APP: () = {
@@ -65,66 +69,139 @@ const APP: () = {
                 b: 49,
             },
         ];
+        let mut elements = Elements::new(60, 15);
+        // Chosen by fair dice roll
+        let mut rand = oorandom::Rand32::new(0);
+        // On average add a new color every 15 steps
+        let mut steps = rand.rand_range(10..20);
         loop {
-            for i in 0..(15 * 3) {
-                // let trail = smart_leds::gamma(
-                //     core::slice::from_ref(&colors[i])
-                //         .into_iter()
-                //         .cycle()
-                //         .take(20)
-                //         .cloned(),
-                // );
-                let trail = smart_leds::gamma(
-                    LedTrail {
-                        color: colors[0],
-                        multiplier: 255,
-                        step: 16,
-                        remaining: 19,
-                    }
-                    .chain(LedTrail {
-                        color: colors[1],
-                        multiplier: 255,
-                        step: 16,
-                        remaining: 19,
-                    })
-                    .chain(LedTrail {
-                        color: colors[2],
-                        multiplier: 255,
-                        step: 16,
-                        remaining: 19,
-                    })
-                    .cycle()
-                    .skip(15 * 3 - 1 - i)
-                    .take(60),
-                );
-                c.resources.ws.write(trail).unwrap();
-                c.resources.delay.delay_ms(50 as u16);
+            steps -= 1;
+            if steps == 0 {
+                steps = rand.rand_range(10..20);
+                elements
+                    .add(colors[rand.rand_range(0..colors.len() as u32) as usize])
+                    .unwrap();
             }
+            elements.step();
+            c.resources
+                .ws
+                .write(smart_leds::gamma(elements.iter()))
+                .unwrap();
+            c.resources.delay.delay_ms(50 as u16);
         }
     }
 };
 
-#[derive(Clone)]
-struct LedTrail {
+struct QueueElement {
     color: RGB8,
-    multiplier: u8,
-    step: u8,
-    remaining: u8,
+    position: u8,
 }
 
-impl Iterator for LedTrail {
+struct Elements {
+    queue: Queue<QueueElement, U16, u8, heapless::spsc::SingleCore>,
+    length: u8,
+    trail_length: u8,
+}
+
+impl Elements {
+    fn new(length: u8, trail_length: u8) -> Self {
+        let queue = unsafe { Queue::u8_sc() };
+
+        Self {
+            queue,
+            length,
+            trail_length,
+        }
+    }
+
+    fn step(&mut self) {
+        let mut i = 0;
+        for x in self.queue.iter_mut() {
+            x.position += 1;
+            i += 1;
+            if i > 100 {
+                panic!("FUU");
+            }
+        }
+        self.cull();
+    }
+
+    fn add(&mut self, color: RGB8) -> Result<(), ()> {
+        let element = QueueElement { color, position: 0 };
+        if self
+            .queue
+            .iter_mut()
+            .next_back()
+            .map(|x| x.position != 0)
+            .unwrap_or(true)
+        {
+            self.queue.enqueue(element).map_err(|_| ())
+        } else {
+            // Too many elements, skip this one
+            Ok(())
+        }
+    }
+
+    // Drop elements that aren't visible anymore
+    fn cull(&mut self) {
+        while self
+            .queue
+            .peek()
+            .map(|x| x.position > (self.length + self.trail_length))
+            .unwrap_or(false)
+        {
+            self.queue.dequeue();
+        }
+    }
+
+    fn iter<'a>(&'a mut self) -> ElementIter<'a> {
+        ElementIter {
+            iter: self.queue.iter().rev().peekable(),
+            pos: 0,
+            step: 255 / self.trail_length,
+            trail_length: self.trail_length,
+            length: self.length,
+        }
+    }
+}
+
+struct ElementIter<'a> {
+    iter: Peekable<Rev<Iter<'a, QueueElement, U16, u8, heapless::spsc::SingleCore>>>,
+    pos: u8,
+    trail_length: u8,
+    step: u8,
+    length: u8,
+}
+
+impl<'a> Iterator for ElementIter<'a> {
     type Item = RGB8;
     fn next(&mut self) -> Option<RGB8> {
-        if self.remaining > 0 {
-            let color = RGB8 {
-                r: (self.color.r as u16 * (self.multiplier as u16 + 1) / 256) as u8,
-                g: (self.color.g as u16 * (self.multiplier as u16 + 1) / 256) as u8,
-                b: (self.color.b as u16 * (self.multiplier as u16 + 1) / 256) as u8,
-            };
-            self.multiplier = self.multiplier.checked_sub(self.step)?;
-            Some(color)
-        } else {
-            None
+        let pos = self.pos;
+        self.pos += 1;
+        // Check if we exceeded the length
+        if pos >= self.length {
+            return None;
         }
+        // Check if it's time for the next element
+        // We don't return now to get results for the full length of the chain
+        if self.iter.peek().map(|x| x.position < pos).unwrap_or(false) {
+            self.iter.next();
+        }
+        if let Some(x) = self.iter.peek() {
+            let distance = x.position - pos;
+            let multiplier = self.trail_length.saturating_sub(distance) as u16 * self.step as u16;
+            Some(brightness(x.color, multiplier))
+        } else {
+            // Return dark pixel
+            Some(RGB8 { r: 0, g: 0, b: 0 })
+        }
+    }
+}
+
+fn brightness(color: RGB8, multiplier: u16) -> RGB8 {
+    RGB8 {
+        r: (color.r as u16 * multiplier / 256) as u8,
+        g: (color.g as u16 * multiplier / 256) as u8,
+        b: (color.b as u16 * multiplier / 256) as u8,
     }
 }
